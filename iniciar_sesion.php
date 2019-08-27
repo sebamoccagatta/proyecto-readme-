dcf4338916454ac4012e3c93f654ba7b9c555c12
milagrosb <milustyles169@gmail.com>
2019-08-27 09:07:51 -0300

    imagenes

--- iniciar_sesion.php
+++ iniciar_sesion.php
@@ -11,12 +11,16 @@
     <title>iniciar sesion</title>
   </head>
   <body>
-<h1>Iniciar sesion.</h1>
-    <label for="Usuario">Nombre de usuario.</label><br>
-    <input type="text" name="Nombre de usuario" required ><br>
-    <label for="password">Contraseña.</label><br>
-    <input type="password" name="Contraseña" required><br>
-    <a href="#">Olvide mi contraseña.</a>
-    <a href="index.html"> <button type="submit" name="Iniciar">Iniciar</button> </a>
+    <h1>Iniciar sesion.</h1>
+    <form class="" action="index.html" method="post">
+
+      <label for="Usuario">Nombre de usuario.</label><br>
+      <input type="text" name="Nombre de usuario" required ><br>
+      <label for="password">Contraseña.</label><br>
+      <input type="password" name="Contraseña" required><br>
+      <a href="#">Olvide mi contraseña.</a>
+      <a href="index.html"> <button type="submit" name="Iniciar">Iniciar</button> </a>
+
+    </form>
   </body>
 </html>
