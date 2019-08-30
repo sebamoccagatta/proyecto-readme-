9306cc4183d9fa0d6ffec664622bdbcf109261aa
milagrosb <milustyles169@gmail.com>
2019-08-30 12:02:05 -0300

    ngmuerign

--- iniciar_sesion.php
+++ iniciar_sesion.php
@@ -7,20 +7,22 @@
 <html>
   <head>
     <meta charset="utf-8">
-      <script src="https://kit.fontawesome.com/5e51a129d6.js"></script>
     <link rel="stylesheet" href="css/stilos.css">
     <title>iniciar sesion</title>
   </head>
   <body>
     <div class="contenedor">
     <h1>Iniciar sesion.</h1>
-    <form class="" action="index.html" method="post">
+    <form class="login" action="index.html" method="post">
 
-      <label for="Usuario">Nombre de usuario.</label><br>
+      <label class="nombredeusuario" for="Usuario">Nombre de usuario.</label><br>
       <input type="text" name="Nombre de usuario" required ><br>
       <label for="password">Contraseña.</label><br>
       <input type="password" name="Contraseña" required><br>
-      <label for="login-remember">Recuerdame</label>
+      <div>
+        <input type="checkbox" name="Recuerdame">
+        <label for="login-remember">Recuerdame</label>
+      </div>
       <a href="#">Olvide mi contraseña.</a>
       <a href="index.html"> <button type="submit" name="Iniciar">Iniciar</button> </a>
 
@@ -51,5 +53,7 @@
       </div>
     </div>
   </footer>
+
+    <script src="https://kit.fontawesome.com/5e51a129d6.js"></script>
   </body>
 </html>
