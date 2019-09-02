866e4b5a7b7456ef3f54c51afb8330755adba059
milagrosb <milustyles169@gmail.com>
2019-09-02 00:05:59 -0300

    cambios al fin :'((((((((((

--- logindex.php
+++ logindex.php
@@ -20,7 +20,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="">Mis artesanias<span class="sr-only">(current)</span></a>
+        <a class="nav-link misartesanias" href="">Mis artesanias<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="register.php"></a>
@@ -31,7 +31,7 @@
       </form>
       <div class="desplegable" id="menu-wrapper">
         <ul id="hmenu">
-          <li><a href="#">Artesano</a>
+          <li class="despliegue"><a href="#"><img src="imagenes/desplegue.png" alt=""></a>
             <ul id="sub-menu">
               <li><a href="#">Mi perfil</a></li>
               <li><a href="#">Configuración</a></li>
